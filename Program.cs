24026fdd46185bd4ccbc6745751bf8c66ae5be77
Bart Duisters <bartduisters@bartduisters.com>
2022-10-12 11:43:58 +0200

    refactor: top level statements - classes capitalized

--- Program.cs
+++ Program.cs
@@ -1,20 +1,43 @@
 ﻿using System;
 
-class voertuig
+Voertuig voertuig = new Voertuig();
+Auto auto = new Auto();
+Vliegtuig vliegtuig = new Vliegtuig();
+Boot boot = new Boot();
+
+Console.WriteLine("Info over het object voertuig van de klasse Voertuig:");
+Console.WriteLine(voertuig.AsText());
+Console.WriteLine();
+
+Console.WriteLine("Info over het object auto van de klasse Auto:");
+Console.WriteLine(auto.AsText());
+Console.WriteLine();
+
+Console.WriteLine("Info over het object vliegtuig van de klasse Vliegtuig:");
+Console.WriteLine(vliegtuig.AsText());
+Console.WriteLine();
+
+Console.WriteLine("Info over het object boot van de klasse boot:");
+Console.WriteLine(boot.AsText());
+Console.ReadKey();
+
+class Voertuig
 {
-    public int wielen {get; set;}
-    public bool KanVliegen {get; set;}
-    public bool KanVaren {get; set;}
+    public int wielen { get; set; }
+    public bool KanVliegen { get; set; }
+    public bool KanVaren { get; set; }
 
     public string AsText()
     {
         string vliegen = "kan niet vliegen";
-        if (KanVliegen){
+        if (KanVliegen)
+        {
             vliegen = "kan vliegen";
 
         }
-         string varen = "kan niet varen";
-        if (KanVaren){
+        string varen = "kan niet varen";
+        if (KanVaren)
+        {
             varen = "kan varen";
 
         }
@@ -24,16 +47,16 @@ class voertuig
 
 }
 
-class auto : voertuig
+class Auto : Voertuig
 {
-    
 
-    public auto ()
+
+    public Auto()
     {
         wielen = 4;
         KanVliegen = false;
         KanVaren = false;
-       
+
     }
 
     public string Talk()
@@ -41,68 +64,34 @@ class auto : voertuig
         return "tuut tuut";
     }
 }
-class vliegtuig : voertuig
+class Vliegtuig : Voertuig
 {
-    
 
-    public vliegtuig ()
+
+    public Vliegtuig()
     {
         wielen = 6;
         KanVliegen = true;
         KanVaren = false;
-        
+
     }
 
-   
-}
-class boot : voertuig
-{
-    
 
-    public boot ()
+}
+class Boot : Voertuig
+{
+
+
+    public Boot()
     {
         wielen = 0;
         KanVliegen = false;
         KanVaren = true;
-        
+
     }
 
-   
+
 }
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        
-        voertuig voertuig = new voertuig();
-        auto auto = new auto();
-        vliegtuig vliegtuig = new vliegtuig();
-        boot boot = new boot();
 
-        Console.WriteLine("Info over het object voertuig van de klasse Voertuig:");
-        
-        Console.WriteLine(voertuig.AsText());
 
-        Console.WriteLine();
-        Console.WriteLine("Info over het object auto van de klasse Auto:");
-        
-        Console.WriteLine(auto.AsText());
-
-        
-
-        Console.WriteLine();
-        Console.WriteLine("Info over het object vliegtuig van de klasse Vliegtuig:");
-        
-        
-        Console.WriteLine(vliegtuig.AsText());
-
-         Console.WriteLine();
-        
-        Console.WriteLine("Info over het object boot van de klasse boot:");
-        
-        Console.WriteLine(boot.AsText());
-
-        Console.ReadKey();
-    }
-}
